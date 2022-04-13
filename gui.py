eb054ef0f3878aa3b48413c51d1f8fd753301c5b
Joseph Baca <Jojobaca123@gmail.com>
2022-04-12 20:03:50 -0600

    Update gui.py

--- gui.py
+++ gui.py
@@ -2,6 +2,7 @@
 # Creating simple GUI to demostrate SYNC capabilities.
 
 import tkinter
+import PIL
 from tkinter import Label, Button, Tk
 from PIL import ImageTk, Image
 import webbrowser
@@ -41,14 +42,14 @@ class CAN_BUS_GUI:
         self.File_button_A2.place(x=525, y=75)
 
         # Structoring Image car
-        self.image = Image.open(self.pwd+"\Images\car.png")
+        self.image = Image.open(self.pwd+"/Images/car.png")
         self.resize_image_car = self.image.resize((330, 255))
         self.resize_image_car = ImageTk.PhotoImage(self.resize_image_car)
         self.image_label = tkinter.Label(image=self.resize_image_car)
         self.image_label.place(x=25, y=150)
 
         # Structoring Image empty car
-        self.image2 = Image.open(self.pwd+"\Images\emptycar.png")
+        self.image2 = Image.open(self.pwd+"/Images/emptycar.png")
         self.resize_image_emptycar = self.image2.resize((330, 255))
         self.resize_image_emptycar = ImageTk.PhotoImage(self.resize_image_emptycar)
         self.image_label2 = tkinter.Label(image=self.resize_image_emptycar)
@@ -60,20 +61,20 @@ class CAN_BUS_GUI:
         image_label2.place(x=400, y=150)
 
         # Copy over text file
-        shutil.copyfile(self.pwd+"\Database\Analyst1\\node_info.txt", 
-        self.pwd+"\Database\Analyst2\\node_info.txt")
+        shutil.copyfile(self.pwd+"/Database/Analyst1/node_info.txt", 
+        self.pwd+"/Database/Analyst2/node_info.txt")
 
         # Copy Json Obj
-        shutil.copyfile(self.pwd+"\Database\Analyst1\\node_info.json", 
-        self.pwd+"\Database\Analyst2\\node_info.json")
+        shutil.copyfile(self.pwd+"/Database/Analyst1/Node_info.json", 
+        self.pwd+"/Database/Analyst2/Node_info.json")
 
     # Command for button Analyst_1
     def read_file(self):
-        webbrowser.open(self.pwd+"\Database\Analyst1\\node_info.txt")
+        webbrowser.open(self.pwd+"/Database/Analyst1/node_info.txt")
 
     # Command for button Analyst_2 
     def read_file_2(self):
-        webbrowser.open(self.pwd+"\Database\Analyst2\\node_info.txt")
+        webbrowser.open(self.pwd+"/Database/Analyst2/node_info.txt")
 
 root = Tk()
 my_gui = CAN_BUS_GUI(root)
